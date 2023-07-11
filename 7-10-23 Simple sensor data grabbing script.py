3b673b260743b8fcd459df530b6d9816958708a0
Gavin <107957601+gavinfunr@users.noreply.github.com>
2023-07-10 17:26:19 -0700

    Update 7-10-23 Simple sensor data grabbing script.py

--- 7-10-23 Simple sensor data grabbing script.py
+++ 7-10-23 Simple sensor data grabbing script.py
@@ -2,15 +2,18 @@ import pandas as pd
 import nidaqmx as ni
 import time
 
-vals = int(input())
+vals = 4 #int(input()) 
 task = ni.task.Task(new_task_name='blue')
 
 task.ai_channels.add_ai_thrmcpl_chan("Dev1/ai0")
 task.start()
-data = task.read(number_of_samples_per_channel=vals,timeout=10)
+df = pd.DataFrame(task.read(number_of_samples_per_channel=vals,timeout=10))
 #time out is how long to wait for samples before returning error
+df = df/100 #converting to amps
+
+path = "C:\\Users\\lapto\\Desktop\\Gavin"
+df.to_csv(path+'LOOK')
 
 
 task.stop()
 task.close()
-print(data)
