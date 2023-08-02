1b8b22de118612cc4d8ac22b302d9b292f4fe198
joshrudfelt <jrudfelt@nevada.unr.edu>
2023-08-02 13:52:37 -0700

    Create LTS_JR.py

---
+++ LTS_JR.py
@@ -0,0 +1,14 @@
+import pylablib as pll
+
+from pylablib.devices import Thorlabs
+Thorlabs.list_kinesis_devices()
+#[('27500001', 'Kinesis K-Cube  DC Driver')]
+#stage = Thorlabs.KinesisMotor("27500001")
+#stage.close()
+
+
+
+
+
+
+print("sup")
