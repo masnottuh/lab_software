4d4c95b53aafa2617db7bf165fd4b1f81acc2caa
joshrudfelt <jrudfelt@nevada.unr.edu>
2023-08-02 17:05:36 -0700

    Update GUI_JR.py
    updated colors, looks pretty

--- GUI_JR.py
+++ GUI_JR.py
@@ -3,8 +3,25 @@ import dearpygui.dearpygui as dpg
 
 HOME_STAGE = 0
 
-dpg.show_debug()
+#dpg.show_debug()
 dpg.create_context()
+dpg.show_style_editor()
+
+with dpg.theme() as item_theme1:
+    with dpg.theme_component(dpg.mvAll):
+        dpg.add_theme_color(dpg.mvThemeCol_Button, (235, 99, 144), category=dpg.mvThemeCat_Core)
+        dpg.add_theme_style(dpg.mvStyleVar_FrameRounding, 0, category=dpg.mvThemeCat_Core)
+
+with dpg.theme() as item_theme_RED:
+    with dpg.theme_component(dpg.mvAll):
+        dpg.add_theme_color(dpg.mvThemeCol_Button, (226, 61, 0), category=dpg.mvThemeCat_Core)
+        dpg.add_theme_style(dpg.mvStyleVar_FrameRounding, 0, category=dpg.mvThemeCat_Core)
+        #dpg.add_theme_style(dpg.mvThemeCol_Text, (0, 0, 0), category=dpg.mvThemeCat_Core)
+
+with dpg.theme() as item_theme_GREEN:
+    with dpg.theme_component(dpg.mvAll):
+        dpg.add_theme_color(dpg.mvThemeCol_Button, (61, 143, 56), category=dpg.mvThemeCat_Core)
+        dpg.add_theme_style(dpg.mvStyleVar_FrameRounding, 0, category=dpg.mvThemeCat_Core)
 
 def popup_funct_home(sender):#function to home stage, after homing HOME_STAGE remains =1 **bug?**
     dpg.configure_item("modal_id", show=False)
@@ -35,7 +52,7 @@ with dpg.window(label="LST Settings", width=400, height=150, pos=(0,0)):
    
     Home = dpg.add_button(label="HOME STAGE", callback=change_state)
     with dpg.popup(dpg.last_item(), mousebutton=dpg.mvMouseButton_Left, modal=True, tag="modal_id"):
-        dpg.add_text("Do you want to home the stage? check if LVIT and rollers are out of the way")
+        dpg.add_text("Do you want to home the stage? check if LVIT and stablizers are out of the way")
         YES_HOME = dpg.add_button(label="Yes, home the stage", callback=popup_funct_home)
         NO_HOME = dpg.add_button(label="Negative, do not home the stage", callback=lambda: dpg.configure_item("modal_id", show=False))
 
@@ -50,12 +67,11 @@ with dpg.window(label="MYDAC", width=400, height=150, pos=(0,150)):
 with dpg.window(width=150, height=150,pos=(400,0)):
     RUN = dpg.add_button(label = "RUN", width=100, height=100, callback=run_function)
 
-with dpg.theme() as item_theme:
-    with dpg.theme_component(dpg.mvAll):
-        dpg.add_theme_color(dpg.mvThemeCol_FrameBg, (200, 200, 100), category=dpg.mvThemeCat_Core)
-        dpg.add_theme_style(dpg.mvStyleVar_FrameRounding, 0, category=dpg.mvThemeCat_Core)
 
-dpg.bind_item_theme(RUN, item_theme)
+dpg.bind_item_theme(RUN, item_theme1)
+dpg.bind_item_theme(Home, item_theme_RED)
+dpg.bind_item_theme(NO_HOME, item_theme_RED)
+dpg.bind_item_theme(YES_HOME, item_theme_GREEN)
 
 dpg.create_viewport(title='Cantilever Interface', width=800, height=600)
 dpg.setup_dearpygui()
