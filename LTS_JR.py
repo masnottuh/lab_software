b3ba9b45a4c748c2cdff1c32220f4b464e377730
joshrudfelt <jrudfelt@nevada.unr.edu>
2023-08-02 18:12:37 -0700

    Update LTS_JR.py

--- LTS_JR.py
+++ LTS_JR.py
@@ -7,16 +7,25 @@ import ctypes
 HWTYPE_LTS300 = 42	# LTS300/LTS150 Long Travel Integrated Driver/Stages
 
 STAGE_UNITS_MM = 1		# Stage units are in mm.
+HOME_FWD = 1
+"""Home in the forward direction"""
+HOME_REV = 2
+"""Home in the reverse direction"""
 
 #motor configs gives device info in tuples
 motor_configs = apt.list_available_devices()
 
 #we grab the serial number from the first tuple
 motor = apt.Motor(motor_configs[0][1])
-
-#now we can move the motor
+print(motor.velocity_upper_limit)
+motor.move_home_direction
+#motor.move_home() #HOMES MOTOR
 # motor.move_home(False)
 # from clr_loader import get_coreclr
 print("Backlash distance: " + str(motor.backlash_distance))
-motor.move_by(0.2)
+#motor.move_by(0)
+print(motor.position)
+motor.move_to(150)
+print(motor.position)
+
 print("Motor position: " + str(motor.position))
