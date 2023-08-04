30de4a99a8a1fa630373ac20a78bbbcfb90c80b2
Gavin <gavinf@nevada.unr.edu>
2023-08-04 12:44:23 -0700

    mydac runs in a function

--- mydac.py
+++ mydac.py
@@ -2,19 +2,21 @@ import nidaqmx
 import matplotlib.pyplot as plt
 from nidaqmx.constants import AcquisitionType
 
+def mydac_collection(samplerate, numsamples):
+    with nidaqmx.Task() as task:
+ #   task.ai_channels.add_ai_voltage_chan("Dev1/ai2")
+     task.ai_channels.add_ai_voltage_chan("Dev1/ai7")
+     task.timing.cfg_samp_clk_timing(samplerate)
 
+     sensor_data = task.read(number_of_samples_per_channel=numsamples)
+     task.close()  
+     print(sensor_data)  
 # Sample Settings
 
-samplerate=20   # Sample Rate in Hz
-numsamples=200
+samplerate=2   # Sample Rate in Hz
+numsamples=10
 
-# sample_mode=AcquisitionType.CONTINUOUS
+mydac_collection((samplerate),(numsamples))
 
-with nidaqmx.Task() as task:
- #   task.ai_channels.add_ai_voltage_chan("Dev1/ai2")
-    task.ai_channels.add_ai_voltage_chan("Dev1/ai7")
-    task.timing.cfg_samp_clk_timing(samplerate)
+# sample_mode=AcquisitionType.CONTINUOUsS
 
-    sensor_data = task.read(number_of_samples_per_channel=numsamples)
-task.close()
-print(sensor_data)
